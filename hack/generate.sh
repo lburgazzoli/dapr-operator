68435fe1fc4f470b0bcb9ac4a56e57689ec40d63
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-05-19 11:01:35 +0200

    scaffold helm operator

---
+++ hack/generate.sh
@@ -0,0 +1,10 @@
+
+
+operator-sdk init --plugins helm \
+	--helm-chart dapr \
+	--helm-chart-repo https://dapr.github.io/helm-charts \
+	--domain dapr.io \
+	--group darp \
+	--version v1alpha1 \
+	--kind Dapr 
+
